aed7b134eb53bf840c9fbcf66a460751b143ae73
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-04-14 05:07:20 +0100

    style edit event page

--- client/src/views/EditEvent.vue
+++ client/src/views/EditEvent.vue
@@ -1,64 +1,134 @@
 <template>
-	<div>
-		<div>
-			<router-link to="/profile">
-				Back to profile
-			</router-link>
+	<div class="edit-event">
+		<div class="form">
+			<div @submit.prevent="" class="form">
+				<div class="card" style="border: none">
+					<h5
+						class="card-header info-color white-text text-center py-3"
+						style="background-color: #17a2bb; color: white;"
+					>
+						<strong>Edit Event</strong>
+					</h5>
+
+					<div class="card-body px-lg-5 pt-0 mt-2">
+						<form class="text-center" style="color: #757575;">
+							<div class="form-row">
+								<div class="col">
+									<div class="md-form">
+										<Datepicker
+											id="date"
+											placeholder="Choose date"
+											v-model="event.date"
+											:full-month-name="true"
+											:format="customFormatter"
+											:input-class="[
+												'browser-default',
+												'custom-select',
+												'mb-4'
+											]"
+										/>
+									</div>
+								</div>
+								<div class="col">
+									<div class="md-form">
+										<select
+											class="browser-default custom-select mb-4"
+											v-model="event.time"
+											id="time"
+										>
+											<option value="" disabled>Choose time</option>
+											<option
+												v-for="time in times"
+												:value="time"
+												:key="time"
+											>
+												{{ time }}
+											</option>
+										</select>
+									</div>
+								</div>
+							</div>
+
+							<select
+								class="browser-default custom-select mb-4"
+								v-model="event.category"
+							>
+								<option value="" disabled>Choose category</option>
+								<option
+									v-for="cat in categories"
+									:value="cat"
+									:key="cat"
+								>
+									{{ cat }}
+								</option>
+							</select>
+
+							<div class="md-form mt-0">
+								<input
+									placeholder="Title"
+									type="text"
+									id="title"
+									class="form-control"
+									v-model="event.title"
+								/>
+							</div>
+
+							<div class="md-form mt-3">
+								<textarea
+									placeholder="Description"
+									id="description"
+									class="form-control md-textarea"
+									rows="3"
+									v-model="event.description"
+								></textarea>
+							</div>
+
+							<div class="form-row mt-3">
+								<div class="col">
+									<div class="md-form">
+										<input
+											type="text"
+											id="location"
+											class="form-control"
+											aria-describedby="location"
+											v-model="event.location"
+										/>
+										<label for="location">Location</label>
+									</div>
+								</div>
+								<div class="col">
+									<div class="md-form">
+										<input
+											type="number"
+											id="number"
+											class="form-control"
+											aria-describedby="number"
+											v-model.number="event.max_guests"
+										/>
+										<label for="number"
+											>Maximum number of guests</label
+										>
+									</div>
+								</div>
+							</div>
+							<button
+								id="btn"
+								class="btn btn-outline-info btn-rounded my-2 waves-effect z-depth-0"
+								@click="editEvent"
+							>
+								Confirm edit
+							</button>
+							<button @click="deleteEvent">
+								<i
+									class="fas fa-trash-alt"
+									style="color: red"
+								></i></button
+							><br />
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
-		<form @submit.prevent="editEvent" style="margin-left: 6em;">
-			<label for="">Title</label><br />
-			<input type="text" placeholder="Title" v-model="event.title" /><br />
-
-			<label for="">Description</label><br />
-			<input
-				type="text"
-				placeholder="Description"
-				v-model="event.description"
-			/><br />
-
-			<label for="">Category</label><br />
-			<select v-model="event.category">
-				<option v-for="cat in categories" :value="cat" :key="cat">
-					{{ cat }}
-				</option> </select
-			><br />
-
-			<label for="">Location</label><br />
-			<input
-				type="text"
-				placeholder="Enter Address"
-				v-model="event.location"
-			/><br />
-
-			<label for="">Date</label><br />
-			<Datepicker
-				placeholder="Enter date"
-				v-model="event.date"
-				:full-month-name="true"
-				:format="customFormatter"
-			/><br />
-
-			<label for="">Time</label><br />
-			<select v-model="event.time">
-				<option v-for="time in times" :value="time" :key="time">
-					{{ time }}
-				</option> </select
-			><br />
-
-			<label for=""> Maximum number of Guests</label><br />
-			<input
-				type="number"
-				placeholder="Enter number of guests"
-				v-model.number="event.max_guests"
-			/><br />
-
-			<button type="submit">Edit Event</button><br />
-		</form>
-		<button @click="deleteEvent">Delete Event</button><br />
-
-		<p v-if="error" class="error">
-			{{ error }}
-		</p>
 	</div>
 </template>
 
@@ -148,4 +218,17 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.edit-event {
+	width: 40%;
+	margin: 0 28%;
+	margin-top: 4%;
+	box-shadow: 1px 2px 1vh #17a2bb;
+	position: absolute;
+}
+
+#btn:hover {
+	background-color: #892c4f;
+	border: solid 1px #892c4f;
+}
+</style>
