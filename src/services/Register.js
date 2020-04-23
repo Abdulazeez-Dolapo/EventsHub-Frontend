bc0c3b7b790ad30ae6911ec4d413f68499c7a2c6
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-04-23 06:36:11 +0100

    fix register bug

--- src/services/Register.js
+++ src/services/Register.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export default () => {
 	const token = sessionStorage.getItem("token")
 	return axios.create({
-		baseURL: `http://localhost:5000/`,
+		baseURL: `https://eventshub-express.herokuapp.com/`,
 		headers: {
 			authorization: `Bearer ${token}`,
 			"Content-Type": `multipart/form-data`,
